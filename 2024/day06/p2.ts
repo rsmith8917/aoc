f820ce34f4458df40962f5ec07aa0044523077e9
rsmith8917 <rsmith8917@gmail.com>
2024-12-06 11:50:09 -0500

    day 6 - part 2 (brute force)

--- 2024/day06/p2.ts
+++ 2024/day06/p2.ts
@@ -1,42 +1,60 @@
 import fs from "node:fs";
 
 // Parsing
-const fileData = fs.readFileSync("./test_input", "utf8");
+const fileData = fs.readFileSync("./input", "utf8");
 
 const rows = fileData.split("\n").filter((r) => !!r);
 const map = rows.map((r) => r.split(""));
 
 // Main
-const { guard, obstacles } = findItems(map);
-const slowGuard = structuredClone(guard);
-let guardAtEdge = false;
-let cycleDetected = false;
-let step = 0;
-while (!guardAtEdge && !cycleDetected) {
+const total = map.length * map[0].length;
+let current = 0;
+let cycleCount = 0;
+for (let i = 0; i < map.length; i++) {
+  for (let j = 0; j < map.length; j++) {
+    console.log((current / total) * 100);
+    const { guard, obstacles } = findItems(map);
+    const newObstacle = { row: i, col: j };
+    if (
+      !isObstacle(newObstacle, obstacles) &&
+      !(guard.row === newObstacle.row && guard.col === newObstacle.col)
+    ) {
+      obstacles.push(newObstacle);
+      const slowGuard = structuredClone(guard);
+      let guardAtEdge = false;
+      let cycleDetected = false;
+      let step = 0;
+      while (!guardAtEdge && !cycleDetected) {
+        moveGuard(guard, obstacles);
+        if (step % 2 === 1) {
+          moveGuard(slowGuard, obstacles);
+        }
+        step = step + 1;
+        guardAtEdge = isGuardAtEdge(guard, map);
+        cycleDetected = guardsMatch(guard, slowGuard);
+      }
+      if (cycleDetected) {
+        cycleCount = cycleCount + 1;
+      }
+    }
+    current = current + 1;
+  }
+}
+
+console.log("Count = ", cycleCount);
+
+// Functions
+function moveGuard(guard: Guard, obstacles: { row: number; col: number }[]) {
   const nextCell = getNextCell(guard);
   if (isObstacle(nextCell, obstacles)) {
     turnGuard(guard);
   } else {
-    moveGuard(guard);
+    stepGuard(guard);
   }
-  if (step % 2 === 0) {
-    const nextCell = getNextCell(slowGuard);
-    if (isObstacle(nextCell, obstacles)) {
-      turnGuard(slowGuard);
-    } else {
-      moveGuard(slowGuard);
-    }
-  }
-  guardAtEdge = isGuardAtEdge(guard, map);
-  cycleDetected = guardsMatch(guard, slowGuard);
 }
-
-console.log(cycleDetected);
-
-// Functions
 function guardsMatch(g1: Guard, g2: Guard) {
   return (
-    g1.row === g2.row && g1.col === g2.col && g2.direction === g2.direction
+    g1.row === g2.row && g1.col === g2.col && g1.direction === g2.direction
   );
 }
 function isGuardAtEdge(guard: Guard, map: string[][]) {
@@ -56,7 +74,7 @@ function isGuardAtEdge(guard: Guard, map: string[][]) {
   return false;
 }
 
-function moveGuard(guard: Guard) {
+function stepGuard(guard: Guard) {
   if (guard.direction === "^") {
     guard.row = guard.row - 1;
     return;
