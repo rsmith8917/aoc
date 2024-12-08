dabcbd4d0eab65f7ace199659455272e5106e5e2
rsmith8917 <rsmith8917@gmail.com>
2024-12-08 14:09:07 -0500

    day 8 - part 1
    day 8 - part 1

---
+++ 2024/day08/p1.ts
@@ -0,0 +1,50 @@
+import fs from "node:fs";
+
+const fileData = fs.readFileSync("./input", "utf8");
+const lines = fileData.split("\n").filter((l) => !!l);
+type Antenna = {
+  row: number;
+  col: number;
+  freq: string;
+};
+const antennas: Antenna[] = [];
+const frequencies = new Set<string>();
+let totalCols = 0;
+for (let row = 0; row < lines.length; row++) {
+  const line = lines[row].split("");
+  totalCols = line.length;
+  for (let col = 0; col < line.length; col++) {
+    const cell = line[col];
+    if (cell !== ".") {
+      antennas.push({ row, col, freq: cell });
+      frequencies.add(cell);
+    }
+  }
+}
+
+const antinodes = new Set<string>();
+for (const frequency of frequencies) {
+  const matchingAntennas = antennas.filter((a) => a.freq === frequency);
+  for (let i = 0; i < matchingAntennas.length; i++) {
+    for (let j = 0; j < matchingAntennas.length; j++) {
+      if (i !== j) {
+        const ant1 = matchingAntennas[i];
+        const ant2 = matchingAntennas[j];
+        const antinode = {
+          row: ant1.row + (ant1.row - ant2.row),
+          col: ant1.col + (ant1.col - ant2.col),
+        };
+
+        if (
+          antinode.row < lines.length &&
+          antinode.row >= 0 &&
+          antinode.col >= 0 &&
+          antinode.col < totalCols
+        ) {
+          antinodes.add(`${antinode.row}:${antinode.col}`);
+        }
+      }
+    }
+  }
+}
+console.log("Count = ", antinodes.size);
